508b2551931d9cf4b9c2b7ae9c1edfd8736aec0d
CodingSolo <codingrising@gmail.com>
2020-03-15 14:12:04 +0400

    Fix eslint, prettier errors

--- src/__tests__/Title.spec.tsx
+++ src/__tests__/Title.spec.tsx
@@ -2,12 +2,11 @@ import React from "react";
 import { render } from "@testing-library/react";
 import Title from "../components/Title";
 
-describe('<Title />', () => {
-    describe('Success', () => {
-        it('render the Title', () => {
-            const { queryByTestId } = render(
-                <Title title="Hello" />
-            );
+describe("<Title />", () => {
+    describe("Success", () => {
+        it("render the Title", () => {
+            const { queryByTestId } = render(<Title title="Hello" />);
+            expect(queryByTestId("title")).toBeTruthy();
         });
     });
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import React, { FC } from 'react';
-import { render } from 'react-dom';
-import App from './App';
-import './global.css'
+import React from "react";
+import { render } from "react-dom";
+import App from "./App";
+import "./global.css";
 
-const root = document.getElementById('root')
+const root = document.getElementById("root");
 
-render(<App />, root)
+render(<App />, root);
