aa7f5d92fe7f927b0b080365cc067d0fb40c5b9d
CodingSolo <codingrising@gmail.com>
2020-09-20 16:29:26 +0400

    fix not a merge function with wepack-merge 5.1.4

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,15 +1,15 @@
-const merge = require ('webpack-merge');
-const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const common = require('./webpack.config');
+const { merge } = require("webpack-merge");
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const common = require("./webpack.config");
 
 const config = {
-    mode : 'production', 
-    devtool: 'source-map',
+    mode: "production",
+    devtool: "source-map",
     output: {
-        path: path.join(__dirname, './build'),
-        filename: 'js/main.[contentHash].js',
-        publicPath: './',
+        path: path.join(__dirname, "./build"),
+        filename: "js/main.[contentHash].js",
+        publicPath: "./",
     },
     plugins: [new CleanWebpackPlugin()],
 };
